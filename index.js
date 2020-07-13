aa13899cbe8b87ecbb7ad7ac0c29e7d8a53ee289
korelstar <korelstar@users.noreply.github.com>
2020-07-13 08:59:49 +0200

    export config depending on environment

--- index.js
+++ index.js
@@ -23,7 +23,4 @@
 const dev = require('./webpack.dev.js')
 const prod = require('./webpack.prod.js')
 
-module.exports = {
-	dev,
-	prod
-}
+module.exports = process.env.NODE_ENV === 'development' ? dev : prod
